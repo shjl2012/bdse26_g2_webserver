07572fd4fde36eb2abb17bcb44272c577b7b7858
shjl2012 <sh.john.lin@gmail.com>
2022-09-11 22:15:23 +0800

    Comments added for better readability

--- webserver_test/copydata.sh
+++ webserver_test/copydata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 把目前路徑的內容複製到 webserver 這個 docker volume 的 /app 資料夾用指令
+# 建立一個用來存檔的中介 container temp，帶複製完資料移除
 docker volume create webserver
 docker container run --name temp -v webserver:/app alpine
 docker cp ./app temp:/app
