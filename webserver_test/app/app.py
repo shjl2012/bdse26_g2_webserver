d8ddd20236a3f3225e8d9b4ef74257208a7783eb
shjl2012 <sh.john.lin@gmail.com>
2022-09-13 18:59:58 +0800

    0913 update

--- webserver_test/app/app.py
+++ webserver_test/app/app.py
@@ -85,5 +85,5 @@ def get_form():
 
 
 
-if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__=="__main__":
+#     app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ webserver_test/app/venv/bin/pct2rgb.py
@@ -0,0 +1,11 @@
+#!/Users/johnlin/Documents/GitHub/bdse26_g2_webserver/webserver_test/app/venv/bin/python3
+
+import sys
+# import osgeo_utils.pct2rgb as a convenience to use as a script
+from osgeo_utils.pct2rgb import *  # noqa
+from osgeo_utils.pct2rgb import main
+from osgeo.gdal import deprecation_warn
+
+
+deprecation_warn('pct2rgb')
+sys.exit(main(sys.argv))
