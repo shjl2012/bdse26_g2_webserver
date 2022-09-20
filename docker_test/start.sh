9a828e2ce5266cacf36e235ec9c36b5471fcd397
shjl2012 <sh.john.lin@gmail.com>
2022-09-20 13:59:30 +0800

    test version

---
+++ docker_test/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+app="johnlin/docker_test_v2"
+docker build -t ${app} .
+docker container run -d -p 80:5000 --name docker_test_v2 ${app}
+
+# 下一步測試
+# 掛載 /app 資料夾到 webserver docker volume
+# docker container run -d -v webserver:/app -p 80:5000 --name docker_test_v2 ${app}
