345d7d58739e9312f8ff1eaf2bcf99a3acdd383f
shjl2012 <sh.john.lin@gmail.com>
2022-09-20 14:05:10 +0800

    renamed folders

--- webserver_test/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-app="johnlin/docker_test_v2.1"
-docker build -t ${app} .
-docker container run -d -p 80:5000 --name docker_test_2 ${app}
-
-# 下一步測試
-# 掛載 /app 資料夾到 webserver docker volume
-# docker container run -d -v webserver:/app -p 80:5000 --name docker_test_v2 ${app}
